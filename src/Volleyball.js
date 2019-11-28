ee56d50299319ba40ad358f1c9264af5854cdac6
Nintoryan <thehappystudio4@gmail.com>
2019-11-28 16:53:37 +0300

    Исправленый прыжок
    Теперь для прыжка нужно нажать на ту клетку где находится гг.

--- src/Volleyball.js
+++ src/Volleyball.js
@@ -41,53 +41,48 @@ States.Volleyball.prototype = {
         group.create(0,0,'volley-back');
         group.create(110,522,'net');
         
+        //Игрок
         player = group.create(pl_left,pl_up,'volley-player');
         player.pivot.x = player.width*2/3;
         player.pivot.y = player.height-30;
         player.animations.add('jump',[0,1,2,2,1,0],10,false);
         player.z = 4;
 
+        //управление кнопками
+        this.add.button(pl_left-200,pl_down-140,'volley-left_down_btn', function (){ButtonControls(pl_left,pl_down);}).moveDown();
+        this.add.button(pl_left -130,pl_up-100,'volley-left_up_btn', function (){ButtonControls(pl_left,pl_up);}).moveDown();
+        this.add.button(pl_right-130,pl_down-138,'volley-right_down_btn', function (){ButtonControls(pl_right,pl_down);}).moveDown();
+        this.add.button(pl_right-130,pl_up-92,'volley-right_up_btn', function (){ButtonControls(pl_right,pl_up);}).moveDown();
 
-        this.add.button(pl_left-200,pl_down-140,'volley-left_down_btn', function (){
+        function ButtonControls(x,y){
             if(canControl){
-                player.x = pl_left;
-                player.y = pl_down;
+                if(player.x == x&&player.y == y){
+                    Jump();
+                }else{
+                    player.x = x;
+                    player.y = y;
+                }
             }
-        }).moveDown();
+        }
         
-        this.add.button(pl_left -130,pl_up-100,'volley-left_up_btn', function (){
-            if(canControl){
-                player.x = pl_left;
-                player.y = pl_up;
-            }
-        }).moveDown();
-        this.add.button(pl_right-130,pl_down-138,'volley-right_down_btn', function (){
-            if(canControl){
-                player.x = pl_right;
-                player.y = pl_down;
-            }
-        }).moveDown();
-        this.add.button(pl_right-130,pl_up-92,'volley-right_up_btn', function (){
-            if(canControl){
-                player.x = pl_right;
-                player.y = pl_up;
-            }
-        }).moveDown();
-
+        //мент
         ment = group.create(enemy_left,enemy_up,'volley-ment');
         ment.pivot.x = ment.width*2/3;
         ment.pivot.y = ment.height-30;
 
+        //'мяч' наркотики
         drugs = group.create(random_x,random_y,'drugs');
         drugs.pivot.x = drugs.width*2/3;
         drugs.pivot.y = drugs.height;
         drugs.scale.setTo(3,3);
         group.sort('y', Phaser.Group.SORT_ASCENDING);
         
-
+        //кнопка назад
         this.add.button(40, 40, 'common-goback', function () {
             game.exitMiniGameSignal.dispatch();
         },this)
+
+        //счёт
         playerScoreText = game.add.text(300,16,'Голунов:0',{fontSize: '32px',fill: 'green'});
         enemyScoreText = game.add.text(500,16,'Мент:0',{fontSize: '32px',fill: 'red'});
         cursors = game.input.keyboard.createCursorKeys();
@@ -95,35 +90,8 @@ States.Volleyball.prototype = {
     
 
     update: function () {
-        if(game.input.activePointer.isDown){ //свайп вверх
-            pointerWasDown = true;
-            firstPointerX = game.input.activePointer.y;
-        }
-        if(pointerWasDown&&game.input.activePointer.isUp){
-            pointerWasDown = false;
-            if(game.input.activePointer.y<firstPointerX){
-                player.animations.play('jump');//прыжок
-                if(player.x==random_x&&player.y==random_y&&canTrow){//Если прыжок на клетке с куда падает мяч
-                    canLoose = false;
-                    var b = game.rnd.integerInRange(0, 1); //50 на 50 будет бросок дальним или ближним
-                    if(b==0){
-                        if(player.x == pl_left){
-                            throwBall(drugs,enemy_left,enemy_up,true);
-                        }else{
-                            throwBall(drugs,enemy_right,enemy_up,true);
-                        }
-                    }else{
-                        if(player.x == pl_left){
-                            throwBall(drugs,enemy_left,enemy_down,true);
-                        }else{
-                            throwBall(drugs,enemy_right,enemy_down,true);
-                        }
-                    }
-                        
-                }
-            }
-        }
-        if(canControl&&(!player.animations.currentAnim.isPlaying && player.animations.currentAnim.name=='jump')){ //Если сейчас не в прыжке
+        //Если сейчас не в прыжке и может управлять
+        if(canControl&&(!player.animations.currentAnim.isPlaying && player.animations.currentAnim.name=='jump')){ 
             if(cursors.left.isDown){ //Предвижение по полю
                 player.x = pl_left
             } else if(cursors.right.isDown){
@@ -134,37 +102,37 @@ States.Volleyball.prototype = {
                 player.y = pl_down
             }
     
+            // прыжок на компе
             if(game.input.keyboard.isDown(Phaser.Keyboard.Z)){
-                player.animations.play('jump');//прыжок
-                if(player.x==random_x&&player.y==random_y&&canTrow){//Если прыжок на клетке с куда падает мяч
-                    canLoose = false;
-                    var b = game.rnd.integerInRange(0, 1); //50 на 50 будет бросок дальним или ближним
-                    if(b==0){
-                        if(player.x == pl_left){
-                            throwBall(drugs,enemy_left,enemy_up,true);
-                        }else{
-                            throwBall(drugs,enemy_right,enemy_up,true);
-                        }
-                    }else{
-                        if(player.x == pl_left){
-                            throwBall(drugs,enemy_left,enemy_down,true);
-                        }else{
-                            throwBall(drugs,enemy_right,enemy_down,true);
-                        }
-                    }
-                        
-                }
-                
+                Jump();
             }
         }
         //Передвижение мента
 
         
     }
-
-
-
 };
+function Jump(){
+    player.animations.play('jump');//прыжок
+        if(player.x==random_x&&player.y==random_y&&canTrow){//Если прыжок на клетке с куда падает мяч
+            canLoose = false;
+            var b = game.rnd.integerInRange(0, 1); //50 на 50 будет бросок дальним или ближним
+            if(b==0){
+                if(player.x == pl_left){
+                    throwBall(drugs,enemy_left,enemy_up,true);
+                }else{
+                    throwBall(drugs,enemy_right,enemy_up,true);
+                }
+            }else{
+                if(player.x == pl_left){
+                    throwBall(drugs,enemy_left,enemy_down,true);
+                }else{
+                    throwBall(drugs,enemy_right,enemy_down,true);
+                }
+            }
+                
+        }
+}
 function throwBall(ball,x,y,GolunovThrow)
 {
     canTrow = false;
