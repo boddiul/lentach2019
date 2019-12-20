51789d6b616165d8b1bc230f14b5843280dcb21b
Nintoryan <thehappystudio4@gmail.com>
2019-12-21 01:07:14 +0300

    Burger+Volley Graphics
    Добавил графон на голунова и бургеры тимати

--- src/Burger.js
+++ src/Burger.js
@@ -22,16 +22,16 @@ States.Burger.prototype = {
         this.add.button(40, 40, 'common-goback', function () {
             game.exitMiniGameSignal.dispatch();
         },this)
-        Text = game.add.text(300,16,'Счёт:0',{fontSize: '32px',fill: 'black'});
+        Text = game.add.text(230,70,'Здоровье Собянина:0',{fontSize: '36px',fill: 'white'});
         peekHoles = [
-                    new PeekHole(230,450),
-                    new PeekHole(480,550),
-                    new PeekHole(120,650),
-                    new PeekHole(320,720),
-                    new PeekHole(90,800),
-                    new PeekHole(480,850),
-                    new PeekHole(300,950),
-                    new PeekHole(60,1030),
+                    new PeekHole(480,300),
+                    new PeekHole(120,350),
+                    new PeekHole(340,450),
+                    new PeekHole(65,550),
+                    new PeekHole(510,575),
+                    new PeekHole(270,650),
+                    new PeekHole(90,750),
+                    new PeekHole(430,785),
                     ];
     },
     update: function () {}
@@ -39,9 +39,9 @@ States.Burger.prototype = {
 
 function Hide(peekHole){
     if(peekHole.isPoop){
-        game.add.tween(peekHole.poop).to( {y:peekHole.y+100}, speed, Phaser.Easing.Linear.None, true)
+        game.add.tween(peekHole.poop).to( {y:peekHole.y+90}, speed, Phaser.Easing.Linear.None, true)
     }else{
-        game.add.tween(peekHole.burger).to( {y:peekHole.y+100}, speed, Phaser.Easing.Linear.None, true);
+        game.add.tween(peekHole.burger).to( {y:peekHole.y+90}, speed, Phaser.Easing.Linear.None, true);
     }
     peekHole.canClick = false;
     randomColdown = game.rnd.realInRange(minColdown,maxColdown);
@@ -53,9 +53,9 @@ function Hide(peekHole){
 
 function Show(peekHole){
     if(peekHole.isPoop){
-        game.add.tween(peekHole.poop).to( {y:peekHole.y-100}, speed, Phaser.Easing.Linear.None, true)
+        game.add.tween(peekHole.poop).to( {y:peekHole.y-50}, speed, Phaser.Easing.Linear.None, true)
     }else{
-        game.add.tween(peekHole.burger).to( {y:peekHole.y-100}, speed, Phaser.Easing.Linear.None, true)
+        game.add.tween(peekHole.burger).to( {y:peekHole.y-50}, speed, Phaser.Easing.Linear.None, true)
     }
     game.time.events.add(Phaser.Timer.SECOND * speed/1000,function(){peekHole.canClick = true},this);
 }
@@ -81,23 +81,24 @@ PeekHole = function(x,y){
     this.x=x;
     this.y=y;
     this.canClick = true;
-    this.burger = game.add.sprite(x+15,y,'burger');
-    this.poop = game.add.sprite(x+15,y,'burger-poop');
-    this.flag = game.add.sprite(x+20,y-100,'burger-flag');
+    this.burger = game.add.sprite(x+10,y+90,'burger');
+    this.poop = game.add.sprite(x+10,y+90,'burger-poop');
+    this.flag = game.add.sprite(x-50,y-100,'burger-flag');
     this.hole = game.add.sprite(x-30,y,'burger-hole');
-    this.hole.moveUp();  
+    this.front = game.add.sprite(x-30,y,'burger-front');
+    this.hole.moveDown();
+    this.hole.moveDown();    
     this.id = ++holesCount;
-    this.burger.scale.x =0.3;
-    this.burger.scale.y= 0.3;
-    this.poop.scale.x = 0.3;
-    this.poop.scale.y = 0.3;
-    this.burger.moveDown();
+    this.burger.moveUp();
+    this.burger.moveUp();
+    this.front.moveUp();
+    this.front.moveUp();
+    this.front.moveUp();
     this.burger.inputEnabled = true;
     this.burger.events.onInputDown.add(function(){ClickNice(this)}, this);
     this.poop.inputEnabled = true;
     this.poop.events.onInputDown.add(function(){ClickFail(this)}, this);
-    this.flag.moveDown();
-    this.flag.moveDown();
+    this.poop.pivot.y = 20;
     this.flag.moveDown();
     Hide(this);
     
@@ -116,9 +117,9 @@ function ClickFail(peekHole){
 }
 function AddScore(){
     score+=100;
-    Text.setText("Cчёт:"+score.toString(10));
+    Text.setText("Здоровье Собянина:"+score.toString(10));
 }
 function DecScore(){
     score-=200;
-    Text.setText("Cчёт:"+score.toString(10));
+    Text.setText("Здоровье Собянина:"+score.toString(10));
 }
